047cbe5cea9166a1caf35147f780adde9a414752
Able Varghese <able.varghese12@gmail.com>
2015-03-21 15:29:54 -0400

    Final_FileIO Program

---
+++ fileIo/FileInputAndOutput.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_Input_Output
+{
+    class FileInputAndOutput
+    {
+        public string fileName;
+        public string fileNameInput;
+        public string pathName;
+        public string relativeDir;
+        public string[] fileArray = new string[3];
+
+     //Method for outputting the data from file which is created
+        public void FileInput()
+        {
+
+            FileStream inFile = new FileStream(this.pathName, FileMode.Create, FileAccess.Write);
+
+            StreamWriter writeFile = new StreamWriter(inFile);
+            writeFile.Write("Jones, Bob: 1 Introduction to Computer Science, A\nJohnson, Sarah: 2 Data Structures, B+\nSmith, Sam: 3 Data Structures, C");
+
+            writeFile.Close();
+            inFile.Close();
+
+        }
+
+     //Method for finding and assigning the path of the file created.
+        public void FilePath()
+        {
+            Console.Write("\nPlease enter the filename for the new file you wish to create:");
+            this.fileName = Console.ReadLine();
+     // getting the relative directory of the program
+            this.relativeDir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+            this.pathName = this.relativeDir + "\\" + this.fileName + ".txt";
+
+        }
+
+      //Method for outputitng the data in the file to the screen
+        public void FileOutput()
+        {
+            try
+            {
+
+                Console.Write("\n\nPlease enter the filename for the file you wanted to read\nNote: Should be same as the one you created before : ");
+                this.fileNameInput = Console.ReadLine();
+                Console.WriteLine("\n\n");
+            
+                this.pathName = this.relativeDir + "\\" + this.fileNameInput + ".txt";
+
+
+                FileStream outFile = new FileStream(pathName, FileMode.Open, FileAccess.Read);
+
+                StreamReader readFile = new StreamReader(outFile);
+
+        //Outputting the data in file row by row
+                Console.WriteLine("%%%%%%%%%%%%%%%%%%%%%OUTPUT%%%%%%%%%%%%%%%%%%%%%%%\n");
+                for (int row = 0; row < 3; row++)
+                    {
+                        string fileData = readFile.ReadLine();
+
+                        this.fileArray[row] = fileData;
+
+                        Console.WriteLine("{0}", fileData);
+                    }
+                Console.WriteLine("\n%%%%%%%%%%%%%%%%%%%%%OUTPUT%%%%%%%%%%%%%%%%%%%%%%%\n\n\n");
+                readFile.Close();
+                outFile.Close();
+            }
+
+    //handling the exception
+            catch (Exception)
+            {
+                Console.WriteLine("*****ERROR: File doesn't exist please try again*****");
+                Console.ReadKey();
+                return;
+            }
+
+        }
+
+
+
+    }
+}
---
+++ fileIo/Program.cs
@@ -0,0 +1,74 @@
+﻿/*Name: Able Varghese
+ *Assignment: 05
+ *Description: This is a program that create, input and read rows from a text file.
+*/
+
+
+
+using System;
+using System.IO;
+
+namespace File_Input_Output
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int choice = 0;
+
+      // Object is created for FileInputAndOutput class
+            FileInputAndOutput FileManipulate = new FileInputAndOutput();
+
+
+      // Menu entry with exception handling
+            try
+            {
+                while (choice != 2)
+                {
+                    Console.WriteLine("\n+++++++++++++++++++++++++++++++++++++++++++");
+                    Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++");
+                    Console.WriteLine("\nPlease enter your choice");
+                    Console.WriteLine("1. Display Grades");
+                    Console.WriteLine("2. Exit");
+                    Console.WriteLine("\n+++++++++++++++++++++++++++++++++++++++++++");
+                    Console.WriteLine("+++++++++++++++++++++++++++++++++++++++++++\n\n");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+
+                    switch (choice)
+                    {
+                        case 1:
+                            {
+                         // Calling methods in FileInputAndOutput class
+                                FileManipulate.FilePath();
+                                FileManipulate.FileInput();
+                                FileManipulate.FileOutput();
+                                break;
+                            }
+                        case 2:
+                            {
+                                return;
+
+                            }
+                        default: Console.WriteLine("Invalid entry, please try again..\n");
+
+                            break;
+                    }
+                }
+            }
+
+
+       //Exception output        
+            catch(Exception)
+                  {
+                        Console.WriteLine("Please enter the integer(1 or 2) from the menu");
+                         return;
+                   }
+        
+
+
+
+        }
+
+    }
+}
